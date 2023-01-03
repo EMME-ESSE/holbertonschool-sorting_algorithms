173e7cb75dd592539c985ff251ed0fb4a81b897e
EMME-ESSE <matimancini@gmail.com>
2023-01-03 08:41:29 -0800

    Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,20 +1,20 @@
 #include "sort.h"
 /*
- * quick_sort - Pass the parameters to the quicksort_recursion function
- * * @array: The array of integers
- * * @size: Size of the array
+* quick_sort - Pass the parameters to the quicksort_recursion function
+* @array: The array of integers
+* @size: Size of the array
 **/
 void quick_sort(int *array, size_t size)
 {
 	quicksort_recursion(array, 0, size - 1);
 }
 /**
- *  * quicksort_recursion - Sorts an array of integers in ascending order
- *    * @array: The array of integers
- *     * @first: first parameter to compare
- *     * @last: last parameter to compare
- *      */
-void quicksort_recursion(int array[],int first,int last)
+ * quicksort_recursion - Sorts an array of integers in ascending order
+ * @array: The array of integers
+ * @first: first parameter to compare
+ * @last: last parameter to compare
+**/
+void quicksort_recursion(int array[], int first, int last)
 {
 	int temp, def, i, j;
 
@@ -22,18 +22,19 @@ void quicksort_recursion(int array[],int first,int last)
 	{
 		def = first;
 		i = first;
-		j = last;	
+		j = last;
 		while (i < j)
 		{
-			while(array[i] <= array[def] && i < last)
+			while (array[i] <= array[def] && i < last)
 				i++;
-			while(array[j] > array[def])
+			while (array[j] > array[def])
 				j--;
-			if(i < j)
+			if (i < j)
 			{
 				temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
+				print_array(array, size);
 			}
 		}
 		temp = array[def];
