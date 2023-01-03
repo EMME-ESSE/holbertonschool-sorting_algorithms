9bb5271cee2016f89a4923e27eecd220bf77530a
EMME-ESSE <matimancini@gmail.com>
2023-01-03 08:32:08 -0800

    Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,10 +10,10 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
+
 		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
-			print_array(array, size);
 			if (array[min_index] > array[j])
 			{
 				min_index = j;
@@ -25,6 +25,7 @@ void selection_sort(int *array, size_t size)
 				array[i] = array[min_index];
 				array[min_index] = temp;
 			}
+			print_array(array, size);
 		}
 	}
 }
